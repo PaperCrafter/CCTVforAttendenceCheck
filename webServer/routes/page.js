06e7d68e29cd7360f6c964e59f3d2596561cba72
papercrafter <whdgml1996@naver.com>
2019-05-14 02:53:26 +0900

    웹서버 밑작업

---
+++ webServer/routes/page.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('./profile', (req, res)=>{
+    res.render('profile', {title:'info - ' ,user:null});
+});
+
+router.get();
