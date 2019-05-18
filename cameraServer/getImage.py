7d02f02723b89cee3f5a9d50a68d87fbb7109622
white Doggy <maro1992@naver.com>
2019-05-18 18:24:16 +0900

    Revert "ServerPart + H\uman detection first fix"
    This reverts commit 834561dc647bac415159228ce05ccfbcb2444e8e.

--- cameraServer/getImage.py
+++ cameraServer/getImage.py
@@ -1,30 +1,9 @@
-# --------- //added part
-from imageai.Detection import ObjectDetection
-import cs
-import os
-# ---------
-
 import io
 import socket
 import struct
+import numpy as np
 from PIL import Image
 
-
-# ---------
-execution_path = os.getcwd()
-detector = ObjectDetection()
-custom = detector.CustomObjects(person=True)
-
-# using YOLOv3 so need yolo.h5 file in excution direction
-detector.setModelTypeAsYOLOv3()
-detector.setModelPath(os.path.join(execution_path, "yolo.h5"))
-detector.loadModel()
-
-num = 0
-Human_num = 0
-Cla = cs.Class("고자",20,19,19)
-
-# ---------
 
 # Start a socket listening for connections on 0.0.0.0:8000 (0.0.0.0 means
 # all interfaces)
@@ -35,7 +14,6 @@ server_socket.listen(0)
 # Accept a single connection and make a file-like object out of it
 connection = server_socket.accept()[0].makefile('rb')
 i = 0
-
 try:
     while True:
         # Read the length of the image as a 32-bit unsigned int. If the
@@ -55,35 +33,10 @@ try:
         image.verify()
 
         image = Image.open(image_stream)
-        image.save('./img/'+str(i)+'img.jpg')
+        image.save('img.jpg')
+        i+=1
+
         print('Image is verified')
-
-        # ---------
-        filename = str(num) + "img.jpg"
-
-        detections = detector.detectCustomObjectsFromImage(custom_objects=custom,
-                                                           input_image=os.path.join('./img/', filename),
-                                                           output_image_path=os.path.join('./img/checked/', filename),
-                                                           minimum_percentage_probability=30)
-        for eachObject in detections:
-            Human_num = Human_num + 1
-
-        # 사람 수 연산 결과와 다음 처리로 이동
-        Cla.change_num(Human_num)
-        if Cla.check(num):
-            connection.send('data for light'.encode('utf-8'))
-            print(Cla.list)
-
-        num = num + 1
-        Human_num = 0
-
-        # 이미지 처리 이후 이상 상황이라고 가정
-
-
-        # ---------
-
-        i = i + 1
-
 finally:
     connection.close()
     server_socket.close()
