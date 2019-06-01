31422b23049222e5fe23b9db525bd3d135939bc0
PaperCrafter <leejonghee342@gmail.com>
2019-06-02 00:24:44 +0900

    implemented pagenation and login page layout

--- webApp/server/api/getList.js
+++ webApp/server/api/getList.js
@@ -4,13 +4,34 @@ const {Post} = require('../models');
 const router = express.Router();
 
 router.get('/', async (req, res, next)=>{
-    const post = await Post.findAll({
-        raw:true,
-    });
 
-    console.log(post);
+    const page = parseInt(req.query.page || 1, 10);
+    
+    if(page<1){
+        res.status=400;
+        return;
+    }
 
-    res.send(post);
+    try{
+        const post = await Post.findAll({
+            limit: 10,
+            order:[['id', 'DESC']],
+            offset:(page-1)*10,
+        });
+
+        let postCount = 0;
+        await Post.count().then(c => {
+            console.log("There are " + c + " projects!");
+            postCount = c;
+        });
+
+        const lastPage = Math.ceil(postCount/10);
+        res.set('last-page', lastPage);
+
+        res.send(post);
+    }catch(e){
+        console.log('err');
+    }
 })
 
 module.exports = router;
